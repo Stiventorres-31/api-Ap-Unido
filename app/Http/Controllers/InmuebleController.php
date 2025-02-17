3b3d73640c19e10d85104f8c21ea96a0ea25249c
Stiven Torres <torresmendozastiven@gmail.com>
2025-02-17 10:13:57 -0500

    Se ha ajustado el tipo de inmueble al momento de registrar un inmueble

--- app/Http/Controllers/InmuebleController.php
+++ app/Http/Controllers/InmuebleController.php
@@ -33,7 +33,7 @@ class InmuebleController extends Controller
     {
         $validator = Validator::make($request->all(), [
             "codigo_proyecto" => "required|exists:proyectos,codigo_proyecto",
-            "tipo_inmueble" => "required|exists:tipo_inmuebles,nombre_tipo_inmueble",
+            "tipo_inmueble_id" => "required|exists:tipo_inmuebles,id",
             "cantidad_inmueble" => "required|integer"
         ]);
 
@@ -42,13 +42,13 @@ class InmuebleController extends Controller
         }
 
         $proyecto = Proyecto::select("id")->where("codigo_proyecto",$request->codigo_proyecto)->first();
-        $tipo_inmueble = Proyecto::select("id")->where("nombre_tipo_inmueble",$request->tipo_inmueble)->first();
+        
 
         try {
             for ($i = 0; $i < $request->cantidad_inmueble; $i++) {
                 Inmueble::create([
                     "proyecto_id" => $proyecto->id,
-                    "tipo_inmueble_id" => $tipo_inmueble->id,
+                    "tipo_inmueble_id" => intval($request->tipo_inmueble_id),
                     "user_id" => Auth::user()->id,
                     "estado" => "A"
                 ]);
